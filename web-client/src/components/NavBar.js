3f0a0eff1a1b5ccdcec2d1a26bd967e10fd63481
AM-77 <amine.griche77@gmail.com>
2019-12-26 16:16:56 +0100

    the nav-br component

---
+++ web-client/src/components/NavBar.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { logout } from './../redux-store/actions/authActionCreator'
+import univ_logo from '../assets/univ-logo.png'
+
+class NavBar extends Component {
+
+    logout = () => {
+        localStorage.removeItem('app_token')
+        this.props.logout()
+    }
+
+    render() {
+        return (
+            <div className="navbar-container">
+                <div className="nav-logo">
+                    <img src={univ_logo} alt="The University's Logo" />
+                </div>
+                {
+                    this.props.islogged
+                        ?
+                        <div className="nav-logged">
+                            <h4 className="title"><b>Account:</b> <span>{this.props.user.email}</span></h4>
+                            <button className="logout-btn" onClick={this.logout}>logout</button>
+                        </div>
+                        :
+                        <div className="nav-links">
+                            <NavLink activeClassName="active" className="nav-link" to="/login">Login</NavLink>
+                            <NavLink activeClassName="active" className="nav-link" to="/subscribe">Subscribe</NavLink>
+                        </div>
+                }
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (store) => {
+    return { ...store }
+}
+
+const dispatchStateToProps = (dispatch) => {
+    return {
+        logout: () => dispatch(logout())
+    }
+}
+
+export default connect(mapStateToProps, dispatchStateToProps)(NavBar)
