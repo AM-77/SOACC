2e0bd1aca253ec198a5cacf89e8d1d1b46d2eb66
AM-77 <amine.griche77@gmail.com>
2019-12-27 00:01:50 +0100

    added errors & success handlers

--- web-client/src/components/Subscribe.js
+++ web-client/src/components/Subscribe.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Axios from 'axios'
 import univ_logo from '../assets/univ-logo.png'
+import { connect } from 'react-redux'
+import { error_handler, clear_error } from '../redux-store/actions/authActionCreator'
 
-export default class Subscribe extends Component {
+class Subscribe extends Component {
 
     constructor(props) {
         super(props)
-        this.state = { email: "", password: "", type: "dep-chef" }
+        this.state = { email: "", password: "", type: "dep-chef", success: null }
     }
 
     onInput = e => {
@@ -18,10 +20,47 @@ export default class Subscribe extends Component {
 
     subscribe = e => {
         e.preventDefault()
-        if (this.state.email !== "" && this.state.password !== "")
+        if (this.state.email !== "" && this.state.password !== "") {
             Axios.post("http://localhost:3301/auth-service/subscribe", { email: this.state.email, password: this.state.password, type: this.state.type, mat: this.state.mat })
-                .then(result => result.data.success ? this.props.history.push("/") : console.log("somthing unnormal happend."))
-                .catch(error => console.log(error))
+                .then(result => {
+                    if (result.data.success) {
+                        this.setState(state => ({ success: { status: "Subscription", message: "The User Has Been Subscribed Successfuly." } }))
+                        this.clear_success()
+                    } else {
+                        this.fire_error({ status_text: "Subscription", message: "Somthing Wrong Happened. Please Try Again Later." })
+                    }
+                })
+                .catch(err => {
+                    const error = {
+                        status: err.response.status,
+                        status_text: err.response.statusText,
+                        message: err.response.data.message
+                    }
+                    this.fire_error(error)
+                })
+        }
+        else {
+            const error = {
+                status: "",
+                status_text: "Empty Fields",
+                message: "Please make sure to fill all the required fields"
+            }
+            this.fire_error(error)
+        }
+    }
+
+    fire_error = error => {
+        this.props.error_handler(error)
+        console.clear()
+        setTimeout(() => {
+            this.props.clear_error()
+        }, 3000)
+    }
+
+    clear_success = () => {
+        setTimeout(() => {
+            this.setState(state => ({ success: null }))
+        }, 3000)
     }
 
     render() {
@@ -30,7 +69,27 @@ export default class Subscribe extends Component {
                 <div className="univ-logo">
                     <img src={univ_logo} alt="The University's Logo" />
                 </div>
-                <h2 className="title">Subscribe</h2>
+                <div className="head">
+                    <h2 className="title">Subscribe</h2>
+                    {
+                        this.props.error
+                            ?
+                            <div className="error-container">
+                                <p className="the-error"><b className="error-status">{this.props.error.status_text}: </b><span className="error-message">{this.props.error.message}</span></p>
+                            </div>
+                            :
+                            null
+                    }
+                    {
+                        this.state.success
+                            ?
+                            <div className="success-container">
+                                <p className="the-success"><b className="success-status">{this.state.success.status}: </b><span className="success-message">{this.state.success.message}</span></p>
+                            </div>
+                            :
+                            null
+                    }
+                </div>
                 <form>
                     <input name="email" type="email" onChange={this.onInput} placeholder="Your email" />
                     <input name="password" type="password" onChange={this.onInput} placeholder="Your password" />
@@ -46,3 +105,11 @@ export default class Subscribe extends Component {
         )
     }
 }
+
+const mapStateToProps = (store) => ({ ...store })
+const dispatchStateToProps = (dispatch) => ({
+    error_handler: (error) => dispatch(error_handler(error)),
+    clear_error: () => dispatch(clear_error())
+})
+
+export default connect(mapStateToProps, dispatchStateToProps)(Subscribe)
