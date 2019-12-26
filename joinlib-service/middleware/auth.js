86c2935d2a37da58b49fa701d7f0a00dd25a8b85
AM-77 <amine.griche77@gmail.com>
2019-12-27 00:07:21 +0100

    changed the JWT_KEY

--- joinlib-service/middleware/auth.js
+++ joinlib-service/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken")
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1]
-        req.verified_token = jwt.verify(token, process.env.JWT_KEY || "GetBooks")
+        req.verified_token = jwt.verify(token, process.env.JWT_KEY || "15zM4%A2%p7$q5Ye1+A0$k4r")
 
         next()
     } catch (error) {
