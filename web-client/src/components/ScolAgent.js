aead2f6eb4a7765f461d1c1f83f41a5c1640b1d1
AM-77 <amine.griche77@gmail.com>
2019-12-27 00:01:38 +0100

    added errors & success handlers

--- web-client/src/components/ScolAgent.js
+++ web-client/src/components/ScolAgent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { error_handler } from '../redux-store/actions/authActionCreator'
+import { error_handler, clear_error } from '../redux-store/actions/authActionCreator'
 import Axios from 'axios'
 
 class ScolAgent extends Component {
@@ -19,7 +19,9 @@ class ScolAgent extends Component {
             phone: null,
             facebook: null,
             twitter: null,
-            second_email: null
+            second_email: null,
+            success: null,
+            userUpdated: false
         }
     }
 
@@ -28,11 +30,16 @@ class ScolAgent extends Component {
         if (mat !== "")
             Axios.get("http://localhost:3301/auth-service/student/" + mat, { headers: { authorization: "Bearer " + this.props.token } })
                 .then(res => {
-                    console.log(res.data.student)
-                    this.setState(state => ({ found_student: res.data.student }))
-                    // Notify the User
+                    this.setState(state => ({ found_student: res.data.student, userUpdated: false }))
                 })
-                .catch(err => this.props.error_handler(err))
+                .catch(err => {
+                    const error = {
+                        status: err.response.status,
+                        status_text: err.response.statusText,
+                        message: err.response.data.message
+                    }
+                    this.fire_error(error)
+                })
     }
 
     onInput = e => {
@@ -46,89 +53,155 @@ class ScolAgent extends Component {
         let value = e.target.value
         let student = this.state.found_student
         student[name] = value
-        this.setState(state => ({ found_student: student }))
+        this.setState(state => ({ found_student: student, userUpdated: true }))
     }
 
     subscribeStudent = e => {
         e.preventDefault()
-        /*    if (this.state.mat
-                && this.state.email
-                && this.state.first_name
-                && this.state.last_name
-                && this.state.birthdate
-                && this.state.year
-                && this.state.dep) {
-                let student = {
-                    mat: this.state.mat,
-                    email: this.state.email,
-                    first_name: this.state.first_name,
-                    last_name: this.state.last_name,
-                    birthdate: this.state.birthdate,
-                    year: this.state.year,
-                    dep: this.state.dep,
-                    links: {
-                        phone: this.state.phone,
-                        facebook: this.state.facebook,
-                        twitter: this.state.twitter,
-                        second_email: this.state.second_email
+        if (this.state.mat
+            && this.state.email
+            && this.state.first_name
+            && this.state.last_name
+            && this.state.birthdate
+            && this.state.year
+            && this.state.dep) {
+            let student = {
+                mat: this.state.mat,
+                email: this.state.email,
+                first_name: this.state.first_name,
+                last_name: this.state.last_name,
+                birthdate: this.state.birthdate,
+                year: this.state.year,
+                dep: this.state.dep,
+                links: {
+                    phone: this.state.phone,
+                    facebook: this.state.facebook,
+                    twitter: this.state.twitter,
+                    second_email: this.state.second_email
+                }
+            }
+            Axios.post("http://localhost:3301/auth-service/student/", student, { headers: { authorization: "Bearer " + this.props.token } })
+                .then(res => {
+                    if (res.data.success) {
+                        this.setState(state => ({ success: { status: "Subscribe", message: "The Student Has Been Subscribed Successfuly." } }))
+                        this.clear_success()
                     }
-                }*/
-        Axios.post("http://localhost:3301/auth-service/student/",/* student */ { headers: { authorization: "Bearer " + this.props.token } })
-            .then(res => {
-                console.log(res)
-                //this.setState(state => ({ found_student: res.data.student }))
-                // Notify the User
-            })
-            .catch(err => this.props.error_handler(err))
-            /*     } else {
-                     console.log("Somthing is missing...")
-                 } */
-            .finally(() => console.log(this.props))
+                })
+                .catch(err => {
+                    const error = {
+                        status: err.response.status,
+                        status_text: err.response.statusText,
+                        message: err.response.data.message
+                    }
+                    this.fire_error(error)
+                })
+        } else {
+            const error = {
+                status: "",
+                status_text: "Empty Fields",
+                message: "Please make sure to fill all the required fields"
+            }
+            this.fire_error(error)
+        }
     }
 
     updateStudent = e => {
         e.preventDefault()
-        /*
-                if (this.state.found_student.mat !== ""
-                    && this.state.found_student.email !== ""
-                    && this.state.found_student.first_name !== ""
-                    && this.state.found_student.last_name !== ""
-                    && this.state.found_student.birthdate !== ""
-                    && this.state.found_student.year !== ""
-                    && this.state.found_student.dep !== "") { */
-        Axios.patch("http://localhost:3301/auth-service/student/" + this.state.found_student.mat, this.state.found_student, { headers: { authorization: "Bearer " + this.props.token } })
-            .then(res => {
-                console.log(res)
-                //this.setState(state => ({ found_student: res.data.student }))
-                // Notify the User
-            })
-            .catch(err => this.props.error_handler(err))
-        /*       } else {
-                   console.log("Somthing is missing...")
-               } */
+
+        if (this.state.found_student.mat !== ""
+            && this.state.found_student.email !== ""
+            && this.state.found_student.first_name !== ""
+            && this.state.found_student.last_name !== ""
+            && this.state.found_student.birthdate !== ""
+            && this.state.found_student.year !== ""
+            && this.state.found_student.dep !== "") {
+            if (this.state.userUpdated)
+                Axios.patch("http://localhost:3301/auth-service/student/" + this.state.found_student.mat, this.state.found_student, { headers: { authorization: "Bearer " + this.props.token } })
+                    .then(res => {
+                        if (res.data.success) {
+                            this.setState(state => ({ success: { status: "Update", message: "The Student Has Been Updated Successfuly." } }))
+                            this.clear_success()
+                        }
+                    })
+                    .catch(err => {
+                        const error = {
+                            status: err.response.status,
+                            status_text: err.response.statusText,
+                            message: err.response.data.message
+                        }
+                        this.fire_error(error)
+                    })
+        } else {
+            const error = {
+                status: "",
+                status_text: "Empty Fields",
+                message: "Please make sure to fill all the required fields"
+            }
+            this.fire_error(error)
+        }
     }
 
     deleteStudent = e => {
         e.preventDefault()
-
         let confirm = window.confirm("Do you really want to delete this student ? \nThis operation can't be undone.")
-
         if (confirm)
             Axios.delete("http://localhost:3301/auth-service/student/" + this.state.found_student.mat, { headers: { authorization: "Bearer " + this.props.token } })
                 .then(res => {
-                    console.log(res)
-                    this.setState(state => ({ found_student: null }))
-                    // Notify the User
+                    if (res.data.success) {
+                        this.setState(state => ({ found_student: null, success: { status: "Delete", message: "The Student Has Been Deleted Successfuly." } }))
+                        this.clear_success()
+                    }
                 })
-                .catch(err => this.props.error_handler(err))
+                .catch(err => {
+                    const error = {
+                        status: err.response.status,
+                        status_text: err.response.statusText,
+                        message: err.response.data.message
+                    }
+                    this.fire_error(error)
+                })
+    }
+
+    fire_error = error => {
+        this.props.error_handler(error)
+        console.clear()
+        setTimeout(() => {
+            this.props.clear_error()
+        }, 3000)
+    }
+
+    clear_success = () => {
+        setTimeout(() => {
+            this.setState(state => ({ success: null }))
+        }, 3000)
     }
 
     render() {
         return (
             <div className="scol-agent-container">
-                <h1>Scol Agent</h1>
+                <div className="head">
+                    <h1>Scol Agent</h1>
+                    {
+                        this.props.error
+                            ?
+                            <div className="error-container">
+                                <p className="the-error"><b className="error-status">{this.props.error.status_text}: </b><span className="error-message">{this.props.error.message}</span></p>
+                            </div>
+                            :
+                            null
+                    }
+                    {
+                        this.state.success
+                            ?
+                            <div className="success-container">
+                                <p className="the-success"><b className="success-status">{this.state.success.status}: </b><span className="success-message">{this.state.success.message}</span></p>
+                            </div>
+                            :
+                            null
+                    }
+                </div>
 
-                <div>
+                <div className="update-container">
                     <h2 className="title">Find & Update A Student</h2>
                     <div>
                         <h4 className="check-label">Search By Matricule: </h4>
@@ -224,9 +297,9 @@ class ScolAgent extends Component {
                     </div>
                 </div>
 
-                <div>
+                <div className="subscribe-student-container">
                     <h2 className="title">Subscribe A Student</h2>
-                    <div>
+                    <div className="subscribe-student-form">
                         <form>
                             <input type="text" onChange={this.onInput} name="mat" placeholder="Enter a student's matricule" />
                             <input type="email" onChange={this.onInput} name="email" placeholder="Enter a student's email" />
@@ -253,6 +326,7 @@ class ScolAgent extends Component {
                             <input type="text" onChange={this.onInput} name="facebook" placeholder="Enter a student's facebook" />
                             <input type="text" onChange={this.onInput} name="twitter" placeholder="Enter a student's twitter" />
                             <input type="email" onChange={this.onInput} name="second_email" placeholder="Enter a student's secondery email" />
+                            <div className="clear"></div>
                             <button onClick={this.subscribeStudent}>Subscribe Student</button>
                         </form>
                     </div>
@@ -263,16 +337,10 @@ class ScolAgent extends Component {
     }
 }
 
-
 const mapStateToProps = (store) => ({ ...store })
-const dispatchStateToProps = (dispatch) => {
-    return {
-        error_handler: (error) => {
-            console.log("Scol agent: ", error)
-            dispatch(error_handler(error))
-        }
-
-    }
-}
+const dispatchStateToProps = (dispatch) => ({
+    error_handler: (error) => dispatch(error_handler(error)),
+    clear_error: () => dispatch(clear_error())
+})
 
 export default connect(mapStateToProps, dispatchStateToProps)(ScolAgent)
