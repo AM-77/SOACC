db96bb96e48719e5c385a04e0b8baf349d8d590e
AM-77 <amine.griche77@gmail.com>
2019-12-27 03:28:03 +0100

    changed stracture and added tabs and the student's list.

--- web-client/src/components/ScolAgent.js
+++ web-client/src/components/ScolAgent.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { error_handler, clear_error } from '../redux-store/actions/authActionCreator'
 import Axios from 'axios'
+import ScolAgentFindStudent from './ScolAgentFindStudent'
+import ScolAgentSubscribeStudent from './ScolAgentSubscribeStudent'
+import ScolAgentStudentsList from './ScolAgentStudentsList'
 
 class ScolAgent extends Component {
 
@@ -21,7 +24,9 @@ class ScolAgent extends Component {
             twitter: null,
             second_email: null,
             success: null,
-            userUpdated: false
+            userUpdated: false,
+            active_tab: "update-student",
+            students: []
         }
     }
 
@@ -176,6 +181,25 @@ class ScolAgent extends Component {
         }, 3000)
     }
 
+    set_active_tab = (active_tab) => {
+        this.setState({ active_tab })
+    }
+
+    componentDidMount() {
+        Axios.get("http://localhost:3301/auth-service/students", { headers: { authorization: "Bearer " + this.props.token } })
+            .then(res => {
+                this.setState(state => ({ students: res.data.students }))
+            })
+            .catch(err => {
+                const error = {
+                    status: err.response.status,
+                    status_text: err.response.statusText,
+                    message: err.response.data.message
+                }
+                this.fire_error(error)
+            })
+    }
+
     render() {
         return (
             <div className="scol-agent-container">
@@ -201,137 +225,26 @@ class ScolAgent extends Component {
                     }
                 </div>
 
-                <div className="update-container">
-                    <h2 className="title">Find & Update A Student</h2>
-                    <div>
-                        <h4 className="check-label">Search By Matricule: </h4>
-                        <div className="update-field">
-                            <label htmlFor="search-mat" className="update-label">matricule: </label>
-                            <input id="search-mat" className="search-input" type="text" onChange={this.findStudent} name="mat" placeholder="Enter a student's matricule" />
-                        </div>
+                <div className="tabs">
+                    <div className="tabs-nav">
+                        <button className={(this.state.active_tab === "update-student") ? 'active-tab' : null} onClick={() => { this.set_active_tab("update-student") }}>Update Student</button>
+                        <button className={(this.state.active_tab === "subscribe-student") ? 'active-tab' : null} onClick={() => { this.set_active_tab("subscribe-student") }}>Subscribe Student</button>
+                        <button className={(this.state.active_tab === "students-list") ? 'active-tab' : null} onClick={() => { this.set_active_tab("students-list") }}>Student's List</button>
                     </div>
-                    <div>
+                    <div className="tabs-body">
                         {
-                            !this.state.found_student
+                            this.state.active_tab === "update-student"
                                 ?
-                                <h4 className="check-label">No Student Was Found</h4>
+                                <ScolAgentFindStudent findStudent={this.findStudent} found_student={this.state.found_student} onUpdateInput={this.onUpdateInput} updateStudent={this.updateStudent} deleteStudent={this.deleteStudent} />
                                 :
-                                <div className="student-found">
-                                    <h4 className="check-label">Search Result: </h4>
-                                    <form>
-
-                                        <div className="update-field">
-                                            <label htmlFor="mat" className="update-label">matricule: </label>
-                                            <input id="mat" defaultValue={this.state.found_student.mat} className="update-input" type="text" onChange={this.onUpdateInput} name="mat" placeholder="Enter a student's matricule" />
-                                        </div>
-
-                                        <div className="update-field">
-                                            <label htmlFor="email" className="update-label">email: </label>
-                                            <input id="email" defaultValue={this.state.found_student.email} className="update-input" type="email" onChange={this.onUpdateInput} name="email" placeholder="Enter a student's email" />
-                                        </div>
-
-                                        <div className="update-field">
-                                            <label htmlFor="first_name" className="update-label">first name: </label>
-                                            <input id="first_name" defaultValue={this.state.found_student.first_name} className="update-input" type="text" onChange={this.onUpdateInput} name="first_name" placeholder="Enter a student's first name" />
-                                        </div>
-
-                                        <div className="update-field">
-                                            <label htmlFor="last_name" className="update-label">last name: </label>
-                                            <input id="last_name" defaultValue={this.state.found_student.last_name} className="update-input" type="text" onChange={this.onUpdateInput} name="last_name" placeholder="Enter a student's last name" />
-                                        </div>
-
-                                        <div className="update-field">
-                                            <label htmlFor="birthdate" className="update-label">birthdate: </label>
-                                            <input id="birthdate" defaultValue={this.state.found_student.birthdate} className="update-input" type="date" min="1960-01-01" max={new Date().getFullYear() - 16 + "-01-01"} onChange={this.onUpdateInput} name="birthdate" placeholder="Enter a student's birthdate" />
-                                        </div>
-
-                                        <div className="update-field">
-                                            <label htmlFor="year" className="update-label">year: </label>
-                                            <select id="year" defaultValue={this.state.found_student.year} className="update-input" name="year" onChange={this.onUpdateInput} placeholder="Enter student's year" >
-                                                <option value="L1">L1</option>
-                                                <option value="L2">L2</option>
-                                                <option value="L3">L3</option>
-                                                <option value="M1">M1</option>
-                                                <option value="M2">M2</option>
-                                                <option value="D1">D1</option>
-                                                <option value="D2">D2</option>
-                                                <option value="D3">D3</option>
-                                            </select>
-                                        </div>
-
-                                        <div className="update-field">
-                                            <label htmlFor="dep" className="update-label">departement: </label>
-                                            <select id="dep" defaultValue={this.state.found_student.dep} className="update-input" name="dep" onChange={this.onUpdateInput} placeholder="Enter a student's departements" >
-                                                <option value="STIW">STIW</option>
-                                                <option value="GL">GL</option>
-                                                <option value="STIC">STIC</option>
-                                                <option value="RSD">RSD</option>
-                                            </select>
-                                        </div>
-
-                                        <div className="update-field">
-                                            <label htmlFor="phone" className="update-label">phone number: </label>
-                                            <input id="phone" defaultValue={this.state.found_student.links.phone} className="update-input" type="phone" onChange={this.onUpdateInput} name="phone" placeholder="Enter a student's phone number" />
-                                        </div>
-
-                                        <div className="update-field">
-                                            <label htmlFor="facebook" className="update-label">facebook: </label>
-                                            <input id="facebook" defaultValue={this.state.found_student.links.facebook} className="update-input" type="text" onChange={this.onUpdateInput} name="facebook" placeholder="Enter a student's facebook" />
-                                        </div>
-
-                                        <div className="update-field">
-                                            <label htmlFor="twitter" className="update-label">twitter: </label>
-                                            <input id="twitter" defaultValue={this.state.found_student.links.twitter} className="update-input" type="text" onChange={this.onUpdateInput} name="twitter" placeholder="Enter a student's twitter" />
-                                        </div>
-
-                                        <div className="update-field">
-                                            <label htmlFor="second_email" className="update-label">secondery email: </label>
-                                            <input id="second_email" defaultValue={this.state.found_student.links.second_email} className="update-input" type="email" onChange={this.onUpdateInput} name="second_email" placeholder="Enter a student's secondery email" />
-                                        </div>
-
-                                        <button onClick={this.updateStudent}>Update Student</button>
-                                        <button className="delete-btn" onClick={this.deleteStudent}>Delete Student</button>
-                                    </form>
-                                </div>
+                                this.state.active_tab === "subscribe-student"
+                                    ?
+                                    <ScolAgentSubscribeStudent onInput={this.onInput} subscribeStudent={this.subscribeStudent} />
+                                    :
+                                    <ScolAgentStudentsList students={this.state.students} />
                         }
                     </div>
                 </div>
-
-                <div className="subscribe-student-container">
-                    <h2 className="title">Subscribe A Student</h2>
-                    <div className="subscribe-student-form">
-                        <form>
-                            <input type="text" onChange={this.onInput} name="mat" placeholder="Enter a student's matricule" />
-                            <input type="email" onChange={this.onInput} name="email" placeholder="Enter a student's email" />
-                            <input type="text" onChange={this.onInput} name="first_name" placeholder="Enter a student's first name" />
-                            <input type="text" onChange={this.onInput} name="last_name" placeholder="Enter a student's last name" />
-                            <input type="date" min="1960-01-01" max={new Date().getFullYear() - 16 + "-01-01"} onChange={this.onInput} name="birthdate" placeholder="Enter a student's birthdate" />
-                            <select name="year" onChange={this.onInput} placeholder="Enter student's year" >
-                                <option value="L1">L1</option>
-                                <option value="L2">L2</option>
-                                <option value="L3">L3</option>
-                                <option value="M1">M1</option>
-                                <option value="M2">M2</option>
-                                <option value="D1">D1</option>
-                                <option value="D2">D2</option>
-                                <option value="D3">D3</option>
-                            </select>
-                            <select name="dep" onChange={this.onInput} placeholder="Enter a student's departements" >
-                                <option value="STIW">STIW</option>
-                                <option value="GL">GL</option>
-                                <option value="STIC">STIC</option>
-                                <option value="RSD">RSD</option>
-                            </select>
-                            <input type="phone" onChange={this.onInput} name="phone" placeholder="Enter a student's phone number" />
-                            <input type="text" onChange={this.onInput} name="facebook" placeholder="Enter a student's facebook" />
-                            <input type="text" onChange={this.onInput} name="twitter" placeholder="Enter a student's twitter" />
-                            <input type="email" onChange={this.onInput} name="second_email" placeholder="Enter a student's secondery email" />
-                            <div className="clear"></div>
-                            <button onClick={this.subscribeStudent}>Subscribe Student</button>
-                        </form>
-                    </div>
-                </div>
-
             </div>
         )
     }
